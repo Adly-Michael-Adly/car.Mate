cd4d1971be47f33bd3c585c918047d88ebf3e3a8
Adly Michael Adly <https://github.com/Adly-Michael-Adly>
2023-05-10 01:36:10 +0200

    Add existing project files to Git

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 import { useState } from "react";
 
 const imageUrl = "https://car-mate-t012.onrender.com/public/img/Products/";
+=======
+import { Rating } from "@mui/material";
+>>>>>>> 4509fc1 (Add existing project files to Git)
 function Product(props){
   const {prodcut}=props;
 
 
+<<<<<<< HEAD
   const [style, setStyle] = useState("card");
   async function Anima (){
       setStyle("card2");
@@ -17,12 +22,25 @@ function Product(props){
       <div className="card-body d-flex flex-column align-items-center text-center" >
         <h5 className="card-title namemarket">{prodcut.Name}</h5>
         <h5 className="card-title text-white namemarket">$ {prodcut.Price}</h5>
+=======
+    return(
+<div  className="card">
+      <img src={prodcut.imageCover} className="card-img-top imagecover" alt={prodcut.Type} />
+      <div className="card-body d-flex flex-column align-items-center text-center pb-0" >
+        <h5 className="card-title namemarket">{prodcut.Name}</h5>
+        <h5 className="card-title text-white namemarket">$ {prodcut.Price}</h5>
+        <Rating className="pt-2 rating" name="read-only" value={prodcut.RatingsAverage} precision={0.1} size="small" readOnly />
+>>>>>>> 4509fc1 (Add existing project files to Git)
       </div>
 
       <div className="card-footer">
         <small className="text-white foter">
           <small className="float-end text-white foter mt-1">Quantity: {prodcut.Quantity}</small>
+<<<<<<< HEAD
           <img src="Condition.png" className="mb-1" alt="Condition" />
+=======
+          <img src="/Condition.png" className="mb-1" alt="Condition" />
+>>>>>>> 4509fc1 (Add existing project files to Git)
           {prodcut.Condition}
           </small>
       </div>
--- src/components/productList2.jsx
+++ src/components/productList2.jsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
-import Product from "./product";
-<<<<<<< HEAD
-=======
+import Product2 from "./product2";
 import { Link } from "react-router-dom";
 
->>>>>>> 4509fc1 (Add existing project files to Git)
-function ProdcutList(){
+function ProdcutList2(){
     const apiurl='https://car-mate-t012.onrender.com/api/v1/prodcuts';
     const [prodcuts,setProducts]=useState([]);
     useEffect(() =>{
@@ -19,21 +16,13 @@ function ProdcutList(){
     return(
         <> 
 
-<<<<<<< HEAD
-<div className="row row-cols-1 col-lg row-cols-md-3 g-5 m-5 cards ">
-    {prodcuts.product?.map((prodcut) =>{
-        return(
-            <div className="col cardp" key={prodcut._id}  >
-                <Product prodcut={prodcut}   />
-=======
-<div id="cards" className="row row-cols-1 col-lg row-cols-md-3 g-5 m-5 cards ">
+<div id="cards" className="row row-cols-1 col-lg-8 row-cols-md-3 g-5 m-5 cards ">
     {prodcuts.product?.map((prodcut) =>{
         return(
             <div className="col cardp" key={prodcut._id}  >
                 <Link to={`/product/${prodcut._id}`}  className="noink" >
-                 <Product prodcut={prodcut}/>
+                 <Product2 prodcut={prodcut}/>
                  </Link>
->>>>>>> 4509fc1 (Add existing project files to Git)
             </div>
         )
     })}
@@ -42,4 +31,4 @@ function ProdcutList(){
 </>
     );
 }
-export default ProdcutList;
+export default ProdcutList2;
